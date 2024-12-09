c04c8b6b7d1c50734aaf3e7d457dc6bece54cceb
Oumar-DIONE <dioneomar014@gmail.com>
2024-12-09 14:55:36 +0000

    integration continue de l'API /dcokerisation

--- test_api.py
+++ test_api.py
@@ -1,9 +1,16 @@
 import requests
+params = {
+        "index": 1,
+        "military": 0,
+        "enhancements": 0,
+        "connevey": 1
+    }
 
 url = "http://127.0.0.1:8000/predict?military=1&index=10&enhancements=0&connevey=1"
-
+url2 = "https://user-odione-90797-0.user.lab.sspcloud.fr/proxy/8000/predict"
 # Effectuer la requête
-response = requests.get(url)
+response = requests.get(url2)
+#response2 = requests.get(url2, params=params)
 
 # Vérifier si la réponse a un statut HTTP valide (200 OK)
 if response.status_code == 200:
